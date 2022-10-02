435224e1a9be88bade581f03f87ce0d586b14642
Ezequiel <zapata.ed1989@gmail.com>
2022-10-02 14:09:03 -0300

    note & nothingselected views

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,17 +1,17 @@
 
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography component="h1">JournalPage</Typography>
-      <Typography >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit recusandae nam numquam fuga voluptatem! Sint deserunt nisi id maxime ducimus, sit natus eveniet consectetur distinctio?</Typography>
 
       {/* Nothing selected*/}
+      {/* <NothingSelectedView /> */}
       
       {/* NoteView */}
+      <NoteView />
 
     </JournalLayout>
   )
---
+++ src/journal/views/index.ts
@@ -0,0 +1,7 @@
+
+
+
+export * from "./NothingSelectedView";
+export * from "./NoteView";
+
+
